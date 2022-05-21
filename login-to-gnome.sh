86bd52a321073df51691a09242a37c69540d384b
Tyler887 <tylermageeshields@gmail.com>
2022-05-21 22:08:12 +0100

    Update login-to-gnome.sh

--- login-to-gnome.sh
+++ login-to-gnome.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-ENTRY=`zenity --password --username --title=$(lsb_release -s -i)`
+USERNAME=`zenity --entry --text='Enter your username.' --title=$(lsb_release -s -i)`
+PASSWD=`zenity --password --text='Welcome, $USERNAME! Enter your password to log in.' --title=$(lsb_release -s -i)`
 
 case $? in
          0)
-	 	echo `$ENTRY | cut -d'|' -f2` | su "`echo $ENTRY | cut -d'|' -f1`" -c gnome-session;;
+	 	echo $PASSWD | su $USERNAME -c gnome-session;;
          1)
-                zenity --warning --title="$(lsb_release -s -i)" --text="Login cancelled. Starting root session for maintenance."
+                zenity --warning --title="$(lsb_release -s -i) [becoming root]" --text="Login cancelled. Starting root session for maintenance."
                 gnome-session
                 ;;
         -1)
-                zenity --error --title="$(lsb_release -s -i)" --text="A fatal error has occured and $(lsb_release -s -i) will now reboot. If this persists, please boot Ubuntu in recovery mode by opening the GRUB bootloader menu."
+                zenity --error --title="$(lsb_release -s -i) [fatal login error]" --text="A fatal error has occured and $(lsb_release -s -i) will now reboot. If this persists, please boot $(lsb_release -i -s) in recovery mode by opening the GRUB bootloader menu."
                 reboot
                 ;;
 esac
